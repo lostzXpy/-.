864976b9148ada236bcde80ee5ec01ae993c840f
LostzX <79033314+lostzXpy@users.noreply.github.com>
2021-04-19 16:24:28 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,21 @@
+echo '\nInstalando python\n'
+
+sleep 2
+
+pkg install python
+pkg install python
+
+echo '\nInstalando nodejs e alguns recursos do npm\n'
+
+sleep 2
+
 apt install nodejs -y
 npm i colors
 npm i crypto
 npm i request
 npm i readline-sync
 npm i cli-progress
+
+echo '\nInstalação completa\n'
+
+sleep 2
