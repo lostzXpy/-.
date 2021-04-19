7c65a5111ff9d0af0372efbbe73b3d8fbed60e98
LostzX <79033314+lostzXpy@users.noreply.github.com>
2021-04-19 16:21:31 -0300

    Update hammer.py

--- tools/hammer.py
+++ tools/hammer.py
@@ -75,14 +75,7 @@ def dos2():
 
 
 def usage():
-	print (''' \033[92m	Hammer Dos Script v.1 http://www.canyalcin.com/
-	It is the end user's responsibility to obey all applicable laws.
-	It is just for server testing script. Your ip is visible. \n
-	usage : python3 hammer.py [-s] [-p] [-t]
-	-h : help
-	-s : server ip
-	-p : port default 80
-	-t : turbo default 135 \033[0m''')
+	print ('\033[1;31m \nError\n')
 	sys.exit()
 
 
