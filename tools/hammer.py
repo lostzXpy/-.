06dc1d9e9d3ab74c89aa72003f5f8de91cbc58ed
LostzX <79033314+lostzXpy@users.noreply.github.com>
2021-04-19 16:31:51 -0300

    Update hammer.py

--- tools/hammer.py
+++ tools/hammer.py
@@ -110,7 +110,7 @@ def get_parameters():
 
 # reading headers
 global data
-headers = open("headers.txt", "r")
+headers = open("tools/headers.txt", "r")
 data = headers.read()
 headers.close()
 #task queue are q,w
